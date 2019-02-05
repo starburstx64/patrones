799e3e8aaa1003fec1ea8736fee0bdf210abbc6a
David Segovia Ley <starburstx64@gmail.com>
2019-02-04 22:31:26 -0600

    Creación del proyecto Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonObject singletonObject = SingletonObject.Instance;
+            singletonObject.DoSomething();
+            SingletonObject.Instance.DoSomething();
+
+            var s = SingletonObject.Instance;
+            s.DoSomething();
+        }
+    }
+}
---
+++ Singleton/Singleton/SingletonObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SingletonObject
+    {
+        private static SingletonObject instance = null;
+        public static SingletonObject Instance
+        {
+            get
+            {
+                Console.WriteLine("Llamando a GetInstance de SingletonObject");
+
+                if (instance == null)
+                {
+                    instance = new SingletonObject();
+                }
+
+                return instance;
+            }
+        }
+
+        private SingletonObject()
+        {
+            Console.WriteLine("Llamando al constructor de SingletonObject");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Llamando a DoSomething de SingletonObject");
+        }
+    }
+}
